a01f34fee79bb3b326fea957d3f9b3eb48647d7a
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-09-10 14:57:28 +0430

    frotan

---
+++ screens/drawer/frotan.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const frotan = () => {
+  return (
+    <View>
+      <Text>frotan</Text>
+    </View>
+  )
+}
+
+export default frotan
